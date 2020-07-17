658f503ea2701ec5e06ab6433ecfedb8b06b9813
Escalona <tescal2@users.noreply.github.com>
2020-07-17 15:34:48 -0400

    Update setup.py

--- 3_predict/source_dir/setup.py
+++ 3_predict/source_dir/setup.py
@@ -2,7 +2,5 @@ from setuptools import setup, find_packages
 
 setup(name='sagemaker-roberta-example',
       version='1.0',
-      description='SageMaker Example for Bert.',
-      author='sofian',
-      author_email='hamitis@amazon.com',
+      description='SageMaker Example for RoBERTa.',
       packages=find_packages(exclude=('tests', 'docs')))
