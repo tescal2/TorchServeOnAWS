0c05b2f16fbb733517a533ff96f0d2e3e26e4f5e
Escalona <tescal2@users.noreply.github.com>
2020-07-20 17:16:30 -0400

    updated roberta example

--- 2_train_model/source_dir/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='sagemaker-roberta-example',
-      version='1.0',
-      description='SageMaker Example for Roberta.',
-      author='sofian',
-      author_email='hamitis@amazon.com',
-      packages=find_packages(exclude=('tests', 'docs')))
