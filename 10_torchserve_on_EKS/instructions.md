dfe5d3c4fac9ebea911f25356393511a9d52dddb
Tim O'Brien <tpobrien@amazon.com>
2020-08-09 11:45:29 -0700

    change lab name

---
+++ 10_torchserve_on_EKS/instructions.md
@@ -0,0 +1,3 @@
+Unstaged changes after reset:
+M	10_torchserve_on_EKS/instructions.md
+D	9_torchserve_on_EKS/instructions.md
---
+++ 10_torchserve_on_EKS/scripts/setup_logging.sh
@@ -0,0 +1,9 @@
+Unstaged changes after reset:
+M	10_torchserve_on_EKS/instructions.md
+M	10_torchserve_on_EKS/scripts/cleanup.sh
+M	10_torchserve_on_EKS/scripts/complete_setup.sh
+M	10_torchserve_on_EKS/scripts/setup_logging.sh
+D	9_torchserve_on_EKS/instructions.md
+D	9_torchserve_on_EKS/scripts/cleanup.sh
+D	9_torchserve_on_EKS/scripts/complete_setup.sh
+D	9_torchserve_on_EKS/scripts/setup_logging.sh
